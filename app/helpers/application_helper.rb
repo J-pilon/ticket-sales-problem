457125edfcf111269fec16847d35bb4595f3d0d5
Josiah <josiah.pilon@gmail.com>
2026-01-02 22:56:26 -0800

    feat: adds EventsController with #index and #show routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def format_event_date(date_string)
+    return "Date not available" if date_string.nil?
+
+    begin
+      # Parse ISO 8601 datetime string
+      datetime = Time.parse(date_string.to_s)
+      # Format as "February 1, 2026 at 10:32 PM"
+      datetime.strftime("%B %-d, %Y at %-l:%M %p")
+    rescue ArgumentError
+      date_string.to_s
+    end
+  end
 end
